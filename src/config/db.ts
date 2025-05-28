525d8f0e50036cecb0c7a08db5934bf15477275b
Samuel <samueladelowo92@gmail.com>
2025-05-29 00:34:59 +0100

    added an env sample file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
-import env from './env';
+import config from './env';
 import logger from '../utils/logger';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(env.MONGODB_URI, {
-      autoIndex: env.NODE_ENV !== 'production',
+    await mongoose.connect(config.MONGODB_URI, {
+      autoIndex: config.NODE_ENV !== 'production',
     });
     logger.info('MongoDB connected successfully');
   } catch (error) {
